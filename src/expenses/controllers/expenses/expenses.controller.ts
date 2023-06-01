f5d06a5db92d239d86b705186998abba6a17464f
Almir Muminovic <projectalmir@gmail.com>
2023-06-01 13:32:33 +0200

    Creating body and params inside of a controller

--- src/expenses/controllers/expenses/expenses.controller.ts
+++ src/expenses/controllers/expenses/expenses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ExpensesService } from 'src/expenses/services/expenses/expenses.service';
 
 @Controller('expenses')
@@ -12,25 +12,30 @@ export class ExpensesController {
     }
 
     @Get(":id")
-    getExpense(@Param("id") id: string){
+    getExpense(@Param("id") id: number){
         return this.expensesService.getExpense()
     }
 
     @Post()
-    createExpense(){
+    createExpense(@Body() expenseData){
+        const {title, amount, description} = expenseData
+        console.log(title, amount, description)
         return this.expensesService.createExpense()
     }
 
-    @Patch()
-    updateExpense(){
+    @Patch(":id")
+    updateExpense(@Body() updateExpenseData, @Param("id") id: number){
+        const {type, amount, description} = updateExpenseData
+        console.log(type, amount, description, id)
         return this.expensesService.updateExpense()
     }
 
-    @Delete()
-    deleteExpense(){
+    @Delete(":id")
+    deleteExpense(@Param("id") id: number){
+        console.log(id)
         return this.expensesService.deleteExpense()
     }
 
-
+    
 
 }
--- src/expenses/services/expenses/expenses.service.ts
+++ src/expenses/services/expenses/expenses.service.ts
@@ -1,29 +1,51 @@
 import { Injectable } from '@nestjs/common';
 
+const expenses = [
+  {
+    type: 'Household',
+    amount: 200,
+    description: 'Groceries',
+  },
+  {
+    type: 'Household',
+    amount: 30,
+    description: 'Fresh Water',
+  },
+  {
+    type: 'Party',
+    amount: 500,
+    description: 'Friday night party out with friends',
+  },
+  {
+    type: 'School',
+    amount: 100,
+    description: 'Students loan',
+  },
+];
+
+let balance = 1000
+
 @Injectable()
 export class ExpensesService {
+  getExpenses() {
+    return expenses;
+  }
 
-    getExpenses(){
-        return "All Expneses from service"
-    }
+  getExpense() {
+    return expenses[0];
+  }
 
+  createExpense() {
+    let amount = 100
+    
+    return 'Create Expense from service';
+  }
 
-    getExpense(){
-        return `Single Expense from service`
-    }
+  updateExpense() {
+    return 'Updated expense from service';
+  }
 
-
-    createExpense(){
-        return "Create Expense from service"
-    }
-
-
-    updateExpense(){
-        return "Updated expense from service"
-    }
-
-
-    deleteExpense(){
-        return "Delete expense from service"
-    }
+  deleteExpense() {
+    return 'Delete expense from service';
+  }
 }
