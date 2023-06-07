67704ceb4bb43c4b42e75c8aa1b7f2e62f83c39f
Almir Muminovic <projectalmir@gmail.com>
2023-06-07 13:54:19 +0200

    Creating helper method

--- src/credit-cards/services/credit-cards/credit-cards.service.ts
+++ src/credit-cards/services/credit-cards/credit-cards.service.ts
@@ -5,38 +5,52 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class CreditCardsService {
+  constructor(
+    @InjectRepository(Credit_card)
+    private creditCardRepository: Repository<Credit_card>,
+  ) {}
 
-    constructor(@InjectRepository(Credit_card) private creditCardRepository: Repository<Credit_card>){}
+  getCreditCards() {
+    return this.creditCardRepository.find();
+  }
 
-    getCreditCards(){
-        return this.creditCardRepository.find() 
+  async getCreditCard(id: number) {
+    const creditCard = await this.creditCardRepository.findOne({
+      where: { id: id },
+    });
+    return creditCard;
+  }
+
+  async createCreditCard(creditCardInfo) {
+    const creditCard = this.creditCardRepository.create(creditCardInfo);
+    return await this.creditCardRepository.save(creditCard);
+  }
+
+  async updateCreditCard(id: number, creditCardInfo: any) {
+    const creditCard = await this.creditCardRepository.update(
+      { id },
+      { ...creditCardInfo },
+    );
+    return creditCard;
+  }
+
+  deleteCreditCard(id: number) {
+    return this.creditCardRepository.delete({ id });
+  }
+
+  // helper method
+  async sumOfAllAmountsOfCards() {
+    const balance = await this.creditCardRepository.sum('amount');
+    return balance;
+  }
+
+  async substractFromCreditCard(id: number, amount: number) {
+    const creditCard = await this.getCreditCard(id);
+    if (!creditCard) {
+      throw new Error('No credit card found');
     }
-
-    async getCreditCard(id: number){
-        console.log("service", id, typeof(id))
-        const creditCard = await this.creditCardRepository.findOne({where: {id: id}})
-        return creditCard
-    }
-
-    async createCreditCard(creditCardInfo){
-        const creditCard = this.creditCardRepository.create(creditCardInfo)
-        return await this.creditCardRepository.save(creditCard)
-    }
-
-    async updateCreditCard(id: number, creditCardInfo: any){
-        
-        const creditCard = await this.creditCardRepository.update({id}, {...creditCardInfo})
-        return creditCard
-    }
-
-    deleteCreditCard(id: number){
-        return this.creditCardRepository.delete({id})
-    }
-
-    // helper method
-    async sumOfAllAmountsOfCards(){
-        const balance = await this.creditCardRepository.sum("amount")
-        return balance
-    }
-
+    const newBalance = creditCard.amount - amount;
+    await this.updateCreditCard(id, newBalance);
+    return 'CreditCard Updated!';
+  }
 }
