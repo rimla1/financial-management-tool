1e1f97405e68417af2e2a36e5fdb7e7c56b1ed72
Almir Muminovic <projectalmir@gmail.com>
2023-06-13 19:11:13 +0200

    Restricting user to have options on type upon creating income

--- src/incomes/dtos/createIncome.dto.ts
+++ src/incomes/dtos/createIncome.dto.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty, IsNumber, IsString, Validate } from 'class-validator'
+import { IsIn, IsNotEmpty, IsNumber, IsString, Validate } from 'class-validator'
 import { PositiveNumberConstraint } from 'src/utils/isPositiveNumberValidator'
 
 export class CreateIncomeDto {
     @IsNotEmpty()
     @IsString()
+    @IsIn(['Job', 'Part Time Job', 'Side Hustle', 'Gift', 'Other'])
     type: string
 
     @IsNotEmpty()
